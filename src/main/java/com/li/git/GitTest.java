4f03e1da2bff7db599e08ccc79c1f8bf547f9e2f
ekko <galois101010@gmail.com>
2022-03-09 15:09:44 +0800

    second commit

--- src/main/java/com/li/git/GitTest.java
+++ src/main/java/com/li/git/GitTest.java
@@ -10,5 +10,6 @@ package com.li.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello world");
     }
 }
