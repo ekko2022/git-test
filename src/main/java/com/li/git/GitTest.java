6200b78f42c17d2e64171d1222226466962daea4
ekko <galois101010@gmail.com>
2022-03-09 15:05:07 +0800

    first commit

---
+++ src/main/java/com/li/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.li.git;
+
+/**
+ * @ClassName GitTest
+ * @Description Git Test Demo
+ * @Author liyang
+ * @Date 2022/3/9 14:59
+ * @Version 1.0.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
