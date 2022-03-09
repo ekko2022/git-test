217e2e7799ddb38e43db403049acbb14bfd07d96
ekko <galois101010@gmail.com>
2022-03-09 15:34:35 +0800

    hot-fix commit1

--- src/main/java/com/li/git/GitTest.java
+++ src/main/java/com/li/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git!!!");
         System.out.println("hello world");
         System.out.println("hello github");
+        System.out.println("hello git hot-fix");
     }
 }
